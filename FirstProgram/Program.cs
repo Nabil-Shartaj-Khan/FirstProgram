6f34d7aeb1dd5e62e936f5a5f790758ccdaad77d
Nabil-Shartaj-Khan <shartajnabil@gmail.com>
2024-04-16 13:58:13 +0600

    1 hour done

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -6,6 +6,7 @@ Console.WriteLine("And the journey begins!");
 Console.WriteLine("I am Nabil!");
 
 //This is a comment
+
 //variable declaration
 
 int x = 2000; //decla_initi
@@ -28,17 +29,74 @@ Console.WriteLine("Converted data: " + b); //use getType() for knowing the data 
 
 //user inputs
 
-Console.WriteLine("Please type your name below- "); //accepting user input
-string name = Console.ReadLine(); //accepting user input
+//Console.WriteLine("Please type your name below- "); //accepting user input
+//string name = Console.ReadLine(); //accepting user input
 
-Console.WriteLine("And your age? ");
-int age= Convert.ToInt32(Console.ReadLine()); //converting user input to int
+//Console.WriteLine("And your age? ");
+//int age= Convert.ToInt32(Console.ReadLine()); //converting user input to int
 
-Console.WriteLine("Nice to meet you " + name + ". And you are " + age + " years old.");
+//Console.WriteLine("Nice to meet you " + name + ". And you are " + age + " years old.");
 
 //to increment use x++(only 1) or x+=(any number you want)
 
 //generating random numbers
+
+Random randomNum = new Random();
+int num= randomNum.Next(1, 7);
+Console.WriteLine(num);
+
+
+//calculating triangle hypotenuse
+
+//taking and converting user input for sideA
+
+/*Console.WriteLine("Provide side A below- ");
+double sideA=Convert.ToDouble(Console.ReadLine());
+
+//taking and converting user input for sideB
+Console.WriteLine("Provide side B below- ");
+double sideB = Convert.ToDouble(Console.ReadLine());
+
+//calculating using formula
+double hyPoTenuse=Math.Sqrt((a*a)+(b*b));
+Console.WriteLine("Calculated hypotenuse: " + hyPoTenuse);
+*/
+
+//string methods
+
+string name = "Nabil Shartaz Khan";
+string phoneNumber = "1-7704-384-40";
+name = name.ToUpper();
+phoneNumber=phoneNumber.Replace("-", ""); //first the to be replaced string then the replacement
+
+string BdNum = phoneNumber.Insert(0, "+880"); //inserting in first index 
+
+//subString extracts the given portion of a string, suppose 0,3 here 0 means first index and 3 means 3 characters after that index.
+
+
+Console.WriteLine("Upper cased name- "+name); //similarly you can use ToLower();
+Console.WriteLine("Replaced number- " + phoneNumber);
+Console.WriteLine("Bangladeshi number is- " + BdNum);
+
+
+//if-else statements
+
+Console.WriteLine("What is your age? ");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 18)
+{
+    Console.WriteLine("Congratulations");
+}
+else if (age < 0)
+{
+    Console.WriteLine("Not a valid age!");
+}
+else {
+    Console.WriteLine("GET OUT!");
+}
+
 
 
 
